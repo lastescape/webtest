27384e220760203e84163fe9db63fe8279a3c452
qdh <bust_nakoruru@163.com>
2018-04-10 19:33:38 +0800

    first commit

---
+++ src/main/java/com.qdh.mvc/controller/QdhmvcController.java
@@ -0,0 +1,20 @@
+package com.qdh.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by qdh on 2018/4/10.
+ */
+@Controller
+public class QdhmvcController {
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    @ResponseBody
+    public String resultOutput(@RequestParam("e") String input) {
+        return input.toUpperCase();
+    }
+}
